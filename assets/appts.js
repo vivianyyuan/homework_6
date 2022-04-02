c0ca6b3b3ac52ad60060f4bb01c533091f0afcd3
Vivian Yuan <vivianyuan@Vivians-MacBook-Pro-3.local>
2022-04-02 18:15:35 -0400

    updated cancel button and made website responsive

---
+++ assets/appts.js
@@ -0,0 +1,110 @@
+// loads the appts in the array on the appointments page
+function loadAppts() {
+    let upcomingText = document.getElementById("upcomingText");
+    var appts = JSON.parse(localStorage.getItem("appts"));
+    let upcoming = document.getElementById("upcoming-appts")
+    console.log(appts);
+    if (appts === null || appts.length == 0) {
+        appts = [];
+        let noAppts = document.createElement("p");
+        noAppts.innerHTML = "You have no upcoming appointments.";
+        upcoming.appendChild(noAppts);
+        upcomingText.innerHTML = "Upcoming Appointments (0)";
+    }
+    else {
+        upcomingText.innerHTML = "Upcoming Appointments" + " (" + appts.length + ")";
+        // iterates through the appts array to display each one in the array
+        for (let i=0; i < appts.length; i++) { 
+            if (appts.length > 0){
+                let appt = document.createElement("div");
+                appt.setAttribute("id", "appt" + i);
+                appt.classList.add("appointment");
+                let type = document.createElement("p");
+                let date = document.createElement("p");
+                let viewDetails = document.createElement("button");
+                let edit = document.createElement("button");
+                let cancel = document.createElement("button");
+                type.innerHTML = "Type: " + appts[i].type;
+                date.innerHTML = "Time: February " + appts[i].date + " @ " + appts[i].time;
+                appt.appendChild(type);
+                appt.appendChild(date);
+                viewDetails.classList.add("smallButton2");
+                viewDetails.style.marginRight = "10px";
+                viewDetails.innerHTML = "View Details";
+                viewDetails.setAttribute("id", "modalBtn");
+
+                // call on apptModal function and pass in appt details
+                viewDetails.setAttribute("onclick", `apptModal("${appts[i].type}", "${appts[i].date}", "${appts[i].time}")`);
+                edit.classList.add("smallButton");
+                edit.style.marginRight = "10px";
+                edit.innerHTML = "Edit";
+                cancel.classList.add("smallButton");
+                cancel.style.marginRight = "10px";
+                cancel.innerHTML = "Cancel";
+                cancel.setAttribute("id", i);
+                cancel.setAttribute("onclick", `cancelAppt("${i}")`);
+                appt.appendChild(viewDetails);
+                appt.appendChild(edit);
+                appt.appendChild(cancel);
+                upcoming.appendChild(appt);
+            }
+        }
+    } 
+}
+
+// creates modal that pops up when view details is clicked
+function apptModal(type, date, time) {
+    var modal = document.getElementById("myModal");
+    var btn = document.getElementById("modalBtn");
+    var span = document.getElementsByClassName("close")[0];
+    let typeM = document.createElement("p");
+    let dateM = document.createElement("p");
+    let location = document.createElement("p");
+    let modalContent = document.getElementById("modalContent");
+    let logistics = document.createElement("p");
+    typeM.innerHTML = "Type: " + type;
+    dateM.innerHTML = "Time: February " + date + " @ " + time;
+    location.innerHTML = "Location: UHS";
+    logistics.innerHTML = "Do not eat or drink anything before the appointment. \nThere will be a $10 fee if you cancel less than 24 hours before your appointment."
+    
+    modalContent.appendChild(typeM);
+    modalContent.appendChild(dateM);
+    modalContent.appendChild(location);
+    modalContent.appendChild(logistics);
+
+    modal.style.display = "block";
+    
+    // click on x or outside modal closes it
+    span.onclick = function() {
+        modal.style.display = "none";
+        document.getElementById("modalContent").innerHTML = "";
+        }
+
+    window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+        document.getElementById("modalContent").innerHTML = "";
+        }
+    }
+}
+
+function cancelAppt(i) {
+    let upcomingText = document.getElementById("upcomingText");
+    var appts = JSON.parse(localStorage.getItem("appts"));
+    let upcoming = document.getElementById("upcoming-appts");
+    appts.splice(i, 1);
+    localStorage.setItem("appts", JSON.stringify(appts));
+    upcomingText.innerHTML = "Upcoming Appointments" + " (" + appts.length + ")";
+    if (appts.length == 0) {
+        let noAppts = document.createElement("p");
+        noAppts.innerHTML = "You have no upcoming appointments.";
+        upcoming.appendChild(noAppts);
+    }
+    let node = document.getElementById("appt" + i);
+    if (node.parentNode) {
+        node.parentNode.removeChild(node);
+    }
+    console.log(appts);
+}
+// ensures that function is always called (appts shown on screen) when the page loads
+loadAppts();
